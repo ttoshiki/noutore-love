2ffaf3613dec02862e791b3f11d8218c53e54af3
t-tamaru <t-tamaru@sakura.ad.jp>
2020-07-21 08:33:00 +0900

    固定ページのテンプレ変更

--- page.php
+++ page.php
@@ -1,22 +1,18 @@
 <?php
+
 get_header();
 ?>
-	<main id="primary" class="site-main">
 
-		<?php
-        while (have_posts()) :
-            the_post();
-
-            get_template_part('template-parts/content', 'page');
-
-            // If comments are open or we have at least one comment, load up the comment template.
-            if (comments_open() || get_comments_number()) :
-                comments_template();
-            endif;
-
-        endwhile; // End of the loop.
-        ?>
-
+	<main class="page__main">
+		<section class="page__inner">
+			<?php while (have_posts()) : ?>
+				<h1><?php the_title(); ?></h1>
+				<?php
+                    the_post();
+                    the_content();
+                endwhile;
+                ?>
+		</section>
 
 <?php
 get_sidebar();
